c584ab0e865e7db8333fad44d1a28f6839ac2b1b
root <root@jbccllpupc610.localhost.com>
2016-10-26 10:21:52 -0500

    LD112244

--- puppet/modules/appdynamics/manifests/init.pp
+++ puppet/modules/appdynamics/manifests/init.pp
@@ -4,25 +4,43 @@ if $hostname =~ /kci|wci|irs/  {
         }
 else { $fname = "jbinst" }
 
-# notify {"==== Validating and OR Installing AppDynamics4.1.4.0 =====":}
+ notify {"==== Validating and OR Installing AppDynamics4.1.4.0 =====":}
+
+$output = generate("/bin/bash", "-c", "/etc/puppet/modules/appdynamics/files/config/CreateXMLfile.sh $hostname")
+ 
+    exec {"appdtest":
+	command => '/bin/true',
+	creates => '/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/readme.txt',
+	path => ["/bin","/usr/bin"]
+       }
 
     file {"/opt/puppet/AppDynamics4.1.4.0.tgz":
-	owner => "$fname",
-	group => "$fname",
-	mode => "0775",
-	ensure => present,
+	ensure => file,
+	mode => "0644",
+	replace => false,
 	source => "puppet:///installer_files/AppDynamics4.1.4.0.tgz",
-	require => File["/opt/puppet"],
-#	require => User["$fname"],File["/opt/puppet"],
-	notify  => Exec["unpack AppDynamics"]
-	}->
-
+	require => [ File["/opt/puppet"], Exec["appdtest"] ],
+#	validate_cmd => '/usr/bin/test ! -d /opt/AppDynamics4.1.4.0'
+	}
 ## Unpack AppD
     exec {"unpack AppDynamics":
-	command => "/bin/tar -zxf /opt/puppet/AppDynamics4.1.4.0.tgz",
-	cwd => "/opt",
-	refreshonly => true
-	}->
+        command => "/bin/tar -zxf /opt/puppet/AppDynamics4.1.4.0.tgz",
+        cwd => "/opt",
+	creates => '/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/readme.txt',
+	require => File["/opt/puppet/AppDynamics4.1.4.0.tgz"]
+        }->
+
+
+   file { "/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/ver4.1.4.0/conf/controller-info.xml":
+        ensure          => file,
+        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_AppAgent",
+#        require         =>  Exec["unpack AppDynamics"],
+        }
+   file { "/opt/AppDynamics4.1.4.0/MachineAgent-4.1.4.0/conf/controller-info.xml":
+        ensure          => file,
+        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_MachineAgent",
+#	require         =>  Exec["unpack AppDynamics"],
+        }
 
 ## Change ownership of the directory
 
@@ -31,32 +49,9 @@ else { $fname = "jbinst" }
         recurse => true,
         owner => "$fname",
         group => "$fname",
-#        mode => "0755"
-        }->
-
-## Create the link
-    file {"/opt/AppDynamics":
-        ensure  => link,
-        target  => 'AppDynamics4.1.4.0',
-    }
-
-$output = generate("/bin/bash", "-c", "/etc/puppet/modules/appdynamics/files/config/CreateXMLfile.sh $hostname")
-
-   file { "/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/ver4.1.4.0/conf/controller-info.xml":
-        ensure          => file,
-        owner           => $fname,
-        group           => $fname,
-        mode            => 750,
-        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_AppAgent",
-#       require         => [ User[$owner], Group[$group] ],
+	require => [ User["$fname"], Exec["unpack AppDynamics"] ],
         }
-   file { "/opt/AppDynamics4.1.4.0/MachineAgent-4.1.4.0/conf/controller-info.xml":
-        ensure          => file,
-        owner           => $fname,
-        group           => $fname,
-        mode            => 750,
-        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_MachineAgent",
-        }
+
    file { "/etc/init.d/appdynamics":
         ensure          => file,
         owner           => root,
@@ -64,7 +59,13 @@ $output = generate("/bin/bash", "-c", "/etc/puppet/modules/appdynamics/files/con
         mode            => 750,
         source          => "puppet:///modules/appdynamics/config/appdynamics_${fname}",
         }
-
+   
+   exec { "AppD_gz clean":
+	command => "cat /dev/null > /opt/puppet/AppDynamics4.1.4.0.tgz",
+	onlyif => "test -f /opt/puppet/AppDynamics4.1.4.0.tgz -a -s /opt/puppet/AppDynamics4.1.4.0.tgz",
+	path => ["/bin","/usr/bin"],
+	require => Exec["unpack AppDynamics"]
+	}
 
 }
 
