6209f0ed7e0bb30b7be34967a18a7eee989434d8
root <root@jbccllpupc610.localhost.com>
2016-10-26 11:23:16 -0500

    LD998877

--- puppet/modules/appdynamics/manifests/init.pp
+++ puppet/modules/appdynamics/manifests/init.pp
@@ -14,32 +14,36 @@ $output = generate("/bin/bash", "-c", "/etc/puppet/modules/appdynamics/files/con
 	path => ["/bin","/usr/bin"]
        }
 
-    file {"/opt/puppet/AppDynamics4.1.4.0.tgz":
+    file {"/tmp/AppDynamics4.1.4.0.tgz":
 	ensure => file,
 	mode => "0644",
 	replace => false,
 	source => "puppet:///installer_files/AppDynamics4.1.4.0.tgz",
-	require => [ File["/opt/puppet"], Exec["appdtest"] ],
-#	validate_cmd => '/usr/bin/test ! -d /opt/AppDynamics4.1.4.0'
+	require => Exec["appdtest"],
+#	require => [ File["/opt/puppet"], Exec["appdtest"] ],
 	}
 ## Unpack AppD
     exec {"unpack AppDynamics":
-        command => "/bin/tar -zxf /opt/puppet/AppDynamics4.1.4.0.tgz",
+        command => "/bin/tar -zxf /tmp/AppDynamics4.1.4.0.tgz",
         cwd => "/opt",
 	creates => '/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/readme.txt',
-	require => File["/opt/puppet/AppDynamics4.1.4.0.tgz"]
+	require => File["/tmp/AppDynamics4.1.4.0.tgz"]
         }->
 
 
    file { "/opt/AppDynamics4.1.4.0/AppServerAgent-4.1.4.0/ver4.1.4.0/conf/controller-info.xml":
-        ensure          => file,
-        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_AppAgent",
-#        require         =>  Exec["unpack AppDynamics"],
+        ensure  => file,
+        owner   => "$fname",
+        group   => "$fname",
+	require => User["$fname"],
+        source  => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_AppAgent",
         }
    file { "/opt/AppDynamics4.1.4.0/MachineAgent-4.1.4.0/conf/controller-info.xml":
-        ensure          => file,
-        source          => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_MachineAgent",
-#	require         =>  Exec["unpack AppDynamics"],
+        ensure  => file,
+        owner   => "$fname",
+        group   => "$fname",
+	require => User["$fname"],
+        source  => "puppet:///modules/appdynamics/config/controller-info.xml_${hostname}_MachineAgent",
         }
 
 ## Change ownership of the directory
@@ -61,8 +65,8 @@ $output = generate("/bin/bash", "-c", "/etc/puppet/modules/appdynamics/files/con
         }
    
    exec { "AppD_gz clean":
-	command => "cat /dev/null > /opt/puppet/AppDynamics4.1.4.0.tgz",
-	onlyif => "test -f /opt/puppet/AppDynamics4.1.4.0.tgz -a -s /opt/puppet/AppDynamics4.1.4.0.tgz",
+	command => "cat /dev/null > /tmp/AppDynamics4.1.4.0.tgz",
+	onlyif => "test -f /tmp/AppDynamics4.1.4.0.tgz -a -s /tmp/AppDynamics4.1.4.0.tgz",
 	path => ["/bin","/usr/bin"],
 	require => Exec["unpack AppDynamics"]
 	}
